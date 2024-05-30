1384c12b1e1f42bd121bd825b7eab5502acab612
Elias Garza Valdés <eliasgarzav@gmail.com>
2024-05-30 02:01:08 -0600

    Added database upload functionality

--- pipe/datawrangling_utils.py
+++ pipe/datawrangling_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pyodbc
 
 def remove_outliers(df, column_name, threshold=3):
     """
@@ -35,3 +36,56 @@ def is_high_season(date):
         (pd.Timestamp(year=year, month=3, day=25) <= date <= pd.Timestamp(year=year, month=4, day=7)):
         return 'temporada alta'
     return 'temporada baja'
+
+
+def get_sql_type(dtype):
+    """Returns a string with the type in sql syntaxis"""
+    
+    if pd.api.types.is_integer_dtype(dtype):
+        return 'INT'
+    elif pd.api.types.is_float_dtype(dtype):
+        return 'FLOAT'
+    elif pd.api.types.is_datetime64_any_dtype(dtype):
+        return 'DATETIME'
+    else:
+        return 'VARCHAR(MAX)'
+    
+def create_table_sql(df, table_name):
+    """Creates a new table based on some df schema"""
+    cols = []
+    for col in df.columns:
+        sql_type = get_sql_type(df[col].dtype)
+        cols.append(f"[{col}] {sql_type}")
+    return f"CREATE TABLE {table_name} ({', '.join(cols)});"
+
+def load_df_to_sql_server(df:pd.DataFrame, table_name:str, conn_str:str, overwrite=False):
+    """Uploads a dataframe to the database"""
+    
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    
+    table_exists_query = f"""
+    IF OBJECT_ID(N'{table_name}', 'U') IS NOT NULL
+        SELECT 1
+    ELSE
+        SELECT 0
+    """
+    
+    cursor.execute(table_exists_query)
+    table_exists = cursor.fetchone()[0]
+    
+    if not table_exists:
+        create_table_query = create_table_sql(df, table_name)
+        cursor.execute(create_table_query)
+        conn.commit()
+    
+    if overwrite:
+        cursor.execute(f"DELETE FROM {table_name}")
+    
+    for index, row in df.iterrows():
+        sql_query = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({', '.join(['?'] * len(row))})"
+        cursor.execute(sql_query, tuple(row))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
